12c093116d475dcb9b3270e7c2c17953ec789895
sailecodes <eliasiv.codes@gmail.com>
2023-09-12 15:53:39 -0700

    add highscore functionality and finish game impl.

--- other/guess my number/script.js
+++ other/guess my number/script.js
@@ -9,6 +9,7 @@ const LOW_GUESS = '📉 Too low';
 
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20 + 1);
@@ -32,6 +33,11 @@ document.querySelector('.check').addEventListener('click', function () {
   }
   // CASE 2: Correct guess
   else if (guess === secretNumber) {
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.number').style.width = '30rem';
