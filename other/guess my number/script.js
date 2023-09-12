1a0b7ac25044bbf04d62ca8417aa644857554ceb
sailecodes <eliasiv.codes@gmail.com>
2023-09-12 14:52:29 -0700

    implement no input case

--- other/guess my number/script.js
+++ other/guess my number/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
+
+/*console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 24;
+console.log(document.querySelector('.guess').value);*/
