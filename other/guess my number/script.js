dc0b0370e0536fda2c8aef5ada6093d16d7fc142
sailecodes <eliasiv.codes@gmail.com>
2023-09-12 15:21:08 -0700

    implement game logic

--- other/guess my number/script.js
+++ other/guess my number/script.js
@@ -1,11 +1,33 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = Number(document.querySelector('.score').textContent);
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else {
+    if (score > 1) {
+      score--;
+      document.querySelector('.score').textContent = score;
+
+      if (guess > secretNumber) {
+        document.querySelector('.message').textContent = '📈 Too high';
+      } else {
+        document.querySelector('.message').textContent = '📉 Too low';
+      }
+    } else if (score == 1) {
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent = '🫠 You lost the game!';
+    }
   }
 });
 
