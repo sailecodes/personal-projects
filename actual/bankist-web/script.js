28b847307ade8f492b3e467052e510e9035e71be
sailecodes <eliasiv.codes@gmail.com>
2023-09-17 23:31:21 -0700

    .

--- actual/bankist-web/script.js
+++ actual/bankist-web/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* DOM elements */
+
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 
@@ -14,6 +16,8 @@ const section1 = document.querySelector('#section--1');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContent = document.querySelectorAll('.operations__content');
+
+/* Modal Functionality */
 
 const openModal = function (e) {
   e.preventDefault();
