a5ee5f8967e0ea2a46416a5463968af14e4dad94
Elias <eiroman@ucsd.edu>
2023-09-18 21:51:49 -0700

    add section reveal

--- actual/bankist-web/script.js
+++ actual/bankist-web/script.js
@@ -151,6 +151,34 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+/* ============================================================================================= */
+/* Section reveal functionality */
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries; // only one threshold
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+/* ============================================================================================= */
+/* Lazy loading functionality */
+
 // ===============================
 
 /* const header = document.querySelector('.header');
