9d6ba1a1711695f1d71ca95b2e525b76f5023078
sailcodes <eliasiv.codes@gmail.com>
2023-09-21 12:55:16 -0700

    add simple fetch functionality

--- actual/forkify/dist/index.e37f48ea.js
+++ actual/forkify/dist/index.e37f48ea.js
@@ -581,8 +581,34 @@ const timeout = function(s) {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
-}; // https://forkify-api.herokuapp.com/v2
- ///////////////////////////////////////
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+const showRecipe = async function() {
+    try {
+        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604691c37cdc054bd034");
+        const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        // DELETE
+        console.log(res, data);
+        let { recipe } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        };
+        // DELETE
+        console.log(recipe);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
 
 },{}]},["aD7Zm","aenu9"], "aenu9", "parcelRequire3a11")
 
