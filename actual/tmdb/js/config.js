d5927bed6e8fce3e04f14614e48fbe37903b3cbe
sailcodes <eliasiv.codes@gmail.com>
2023-09-25 21:29:52 -0700

    start nav code

---
+++ actual/tmdb/js/config.js
@@ -0,0 +1,7 @@
+export const API_KEY = "40d476b7bf5ff2313ce9a96c31065e66";
+export const API_RAT =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MGQ0NzZiN2JmNWZmMjMxM2NlOWE5NmMzMTA2NWU2NiIsInN1YiI6IjY1MTIyNTYzMjZkYWMxMDE0ZTIzMzM4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ol_KoRpPN_9jMnmWMVoAklqxoxIvq9uRoKatzqDiTXY";
+export const BASE_URL = "https://api.themoviedb.org/3/";
+export const BASE_URL_IMAGE = "https://image.tmdb.org/t/p/";
+
+export const BACKDROP_SIZE = "w300";
---
+++ actual/tmdb/js/model.js
@@ -0,0 +1,7 @@
+import { API_KEY, API_RAT, BASE_URL, BASE_URL_IMAGE, BACKDROP_SIZE } from "./config.js";
+
+export const state = {};
+
+export const test = function () {
+  console.log("TESTING: ", API_KEY);
+};
