434bc1822bfe4e668a3e3af463af041d148e7d37
sailecodes <eliasiv.codes@gmail.com>
2023-09-28 02:20:30 -0700

    add dynamic slider in spotlight

--- actual/tmdb/src/js/views/spotlightView.js
+++ actual/tmdb/src/js/views/spotlightView.js
@@ -2,44 +2,96 @@ class SpotlightView {
   #spotlightBtn = document.querySelectorAll(".content-spotlight--btn");
   #sliderElements = document.querySelectorAll(".content-spotlight--main-content");
   #sliderMaxElements = this.#sliderElements.length;
+  #currentSlide = 0;
   #markerContainer = document.querySelector(".content-spotlight--markers");
 
   initHandlers() {
-    this.#addSliderMoveHandler();
+    this.#addTransitionByBtnHandler();
+    this.#addTransitionByKeyHandler();
+    this.#addTransitionByMarkerHandler();
+  }
+
+  #addTransitionByBtnHandler() {
+    this.#spotlightBtn.forEach(this.#addTransitionByBtnCallerHelper.bind(this));
+  }
+
+  #addTransitionByBtnCallerHelper(element) {
+    element.addEventListener("click", this.#addTransitionByBtn.bind(this));
+  }
+
+  #addTransitionByBtn(e) {
+    if (e.currentTarget.classList.contains("content-spotlight--left-btn")) this.#prevSlide();
+    else if (e.currentTarget.classList.contains("content-spotlight--right-btn")) this.#nextSlide();
+  }
+
+  #addTransitionByKeyHandler() {
+    document.addEventListener("keydown", this.#addTransitionByKey.bind(this));
+  }
+
+  #addTransitionByKey(e) {
+    if (e.key === "ArrowLeft") this.#prevSlide();
+    else if (e.key === "ArrowRight") this.#nextSlide();
+  }
+
+  #addTransitionByMarkerHandler() {
+    this.#markerContainer.addEventListener("click", this.#addTransitionByMarker.bind(this));
+  }
+
+  #addTransitionByMarker(e) {
+    if (e.target.classList.contains("content-spotlight--marker")) {
+      const slide = e.target.dataset.slide;
+
+      this.#activateMarker(slide);
+      this.#transitionNextSlide(slide);
+    }
   }
 
   initDefaultState() {
     this.#initMarkers();
-    this.#activateMarker();
-  }
-
-  #addSliderMoveHandler() {
-    this.#spotlightBtn.forEach((element) => {
-      element.addEventListener("click", function (e) {
-        if (e.currentTarget.classList.contains("content-spotlight--left-btn")) {
-          console.log("LEFT BUTTON CLICKED");
-        } else if (e.currentTarget.classList.contains("content-spotlight--right-btn")) {
-          console.log("RIGHT BUTTON CLICKED");
-        }
-      });
-    });
+    this.#activateMarker(0);
+    this.#transitionNextSlide(0);
   }
 
   #initMarkers() {
     this.#sliderElements.forEach(this.#initMarkersHelper.bind(this));
   }
 
-  #initMarkersHelper(value, index) {
-    this.#markerContainer.insertAdjacentHTML("beforeend", `<div class="content-spotlight--marker"></div>`);
+  #initMarkersHelper(_, index) {
+    this.#markerContainer.insertAdjacentHTML(
+      "beforeend",
+      `<div class="content-spotlight--marker" data-slide=${index}></div>`
+    );
   }
 
   #activateMarker(slide) {
-    let firstMarker = document.querySelector(".content-spotlight--marker");
+    document
+      .querySelectorAll(".content-spotlight--marker")
+      .forEach((value) => value.classList.remove("content-spotlight--marker-active"));
 
-    firstMarker.classList.add("content-spotlight--marker-active");
+    document
+      .querySelector(`.content-spotlight--marker[data-slide="${slide}"]`)
+      .classList.add("content-spotlight--marker-active");
   }
 
-  #transitionNextSlide() {}
+  #transitionNextSlide(slide) {
+    this.#sliderElements.forEach((value, index) => (value.style.transform = `translateX(${(index - slide) * 100}%)`));
+  }
+
+  #nextSlide() {
+    if (this.#currentSlide === this.#sliderMaxElements - 1) this.#currentSlide = 0;
+    else this.#currentSlide++;
+
+    this.#activateMarker(this.#currentSlide);
+    this.#transitionNextSlide(this.#currentSlide);
+  }
+
+  #prevSlide() {
+    if (this.#currentSlide === 0) this.#currentSlide = this.#sliderMaxElements - 1;
+    else this.#currentSlide--;
+
+    this.#activateMarker(this.#currentSlide);
+    this.#transitionNextSlide(this.#currentSlide);
+  }
 }
 
 export default new SpotlightView();
