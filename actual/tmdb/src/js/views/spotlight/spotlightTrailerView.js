f42d57a75d808cea55fe3e546f643423522f0a95
sailecodes <eliasiv.codes@gmail.com>
2023-10-10 23:55:06 -0700

    fix spotlight trailer transition bug

--- actual/tmdb/src/js/views/spotlight/spotlightTrailerView.js
+++ actual/tmdb/src/js/views/spotlight/spotlightTrailerView.js
@@ -12,17 +12,13 @@ class SpotlightTrailerView {
   addOnTrailerBtnClickedHandler() {
     this.#trailerBtns.forEach((btn) => {
       btn.addEventListener("click", (e) => {
-        console.log(e.target, e.currentTarget);
         const trailerIF = e.currentTarget
           .closest(".content-spotlight--main-content")
           .querySelector(".content-spotlight--trailer");
         const trailerBtnText = e.currentTarget.querySelector(".content-spotlight--trailer-text");
 
-        if (this.#trailerPlayingFlag) {
-          this.#resetTrailer(trailerIF, trailerBtnText);
-        } else {
-          this.#playTrailer(trailerIF, trailerBtnText);
-        }
+        if (this.#trailerPlayingFlag) this.#resetTrailer(trailerIF, trailerBtnText);
+        else this.#playTrailer(trailerIF, trailerBtnText);
 
         this.#trailerPlayingFlag = !this.#trailerPlayingFlag;
       });
@@ -32,35 +28,40 @@ class SpotlightTrailerView {
   addOnSpotlightBtnClickedHandler() {
     this.#spotlightBtns.forEach((btn) => {
       btn.addEventListener("click", () => {
-        setTimeout(() => {
-          const activeSlide = document.querySelector(".content-spotlight--marker-active").dataset.slide;
-          const mainContent = document.querySelectorAll(".content-spotlight--main-content")[activeSlide];
-          const trailerIF = mainContent.querySelector(".content-spotlight--trailer");
-          const trailerBtnText = mainContent.querySelector(".content-spotlight--trailer-text");
+        const activeSlide = document.querySelector(".content-spotlight--marker-active").dataset.slide;
+        const prevContent = document.querySelectorAll(".content-spotlight--main-content")[activeSlide];
+        const trailerIF = prevContent.querySelector(".content-spotlight--trailer");
+        const trailerBtnText = prevContent.querySelector(".content-spotlight--trailer-text");
 
-          this.#resetTrailer(trailerIF, trailerBtnText);
-        }, 100);
+        this.#trailerPlayingFlag = true;
+        this.#resetTrailer(trailerIF, trailerBtnText);
+        this.#trailerPlayingFlag = false;
       });
     });
   }
 
   #resetTrailer(trailerIF, trailerBtnText) {
     trailerBtnText.textContent = "Watch trailer";
+
     this.#changeStyleAttributes(trailerIF);
-    trailerIF.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', "*");
+    trailerIF.contentWindow.postMessage('{"event":"command", "func":"stopVideo", "args":""}', "*");
   }
 
   #playTrailer(trailerIF, trailerBtnText) {
     trailerBtnText.textContent = "Pause trailer";
+
     this.#changeStyleAttributes(trailerIF);
+
     trailerIF.allow = "autoplay";
     trailerIF.src = trailerIF.src.includes("&autoplay=1") ? trailerIF.src : trailerIF.src.concat("&autoplay=1");
   }
 
   #changeStyleAttributes(trailerIF) {
+    console.log("changing attributes", this.#trailerPlayingFlag);
     trailerIF.style.transitionDuration = this.#trailerPlayingFlag ? "0.1s" : "1s";
     trailerIF.style.transitionDelay = this.#trailerPlayingFlag ? "0s" : "1s";
     trailerIF.style.opacity = this.#trailerPlayingFlag ? "0" : "1";
+    console.log(trailerIF);
   }
 }
 
