57231abc45c24d336407f98b5beeae16932b248c
sailecodes <eliasiv.codes@gmail.com>
2023-10-20 14:07:39 -0700

    init

---
+++ websites/bible-tracker/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <SectionNav />
+      <Checklist />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
