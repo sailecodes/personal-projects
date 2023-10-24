294ead9b6e907ea55504996a7d37caa644b19618
sailecodes <eliasiv.codes@gmail.com>
2023-10-24 16:33:50 -0700

    init visual container layout

--- websites/portfolios/russel-portfolio/src/js/components/Projects.js
+++ websites/portfolios/russel-portfolio/src/js/components/Projects.js
@@ -10,8 +10,45 @@ const Projects = () => {
             <p className="Projects--description">
               An exciting deck-building adventure that focuses on four unique heroes with different cards and relics.
             </p>
+            {/* <div className="Projects--roles-container">
+              <p>Roles</p>
+              <ul className="Projects--roles">
+                <li>Creating gameplay elements</li>
+                <li>Designing character cards and relics</li>
+                <li>Balancing character skillsets</li>
+                <li>Testing gameplay flow and loop</li>
+              </ul>
+            </div> */}
           </div>
           <div className="Projects--visual-container">
+            <button className="Projects--arrow-btn Projects--arrow-btn-left">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 19.5L8.25 12l7.5-7.5"
+                />
+              </svg>
+            </button>
+            <button className="Projects--arrow-btn Projects--arrow-btn-right">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </button>
             <img
               className="Projects--img"
               src="/img/projects/aeyos/aeyos-1.png"
@@ -39,9 +76,6 @@ const Projects = () => {
             /> */}
           </div>
         </div>
-
-        <div className="Projects--meta-container">Hello</div>
-        <div className="Projects--meta-container">Hello</div>
       </main>
     </div>
   );
