c12cd1b1755f425c5e78aa1a1cb282aed4449022
sailecodes <eliasiv.codes@gmail.com>
2023-10-24 17:39:13 -0700

    dynamically add project data into project section

--- websites/portfolios/russel-portfolio/src/js/components/Projects.js
+++ websites/portfolios/russel-portfolio/src/js/components/Projects.js
@@ -1,81 +1,64 @@
 import "../../css/Projects.css";
+import projectData from "../data/projectData";
 
 const Projects = () => {
+  console.log(projectData);
+
   return (
     <div className="Projects--container">
       <main className="Projects">
-        <div className="Projects--meta-container">
-          <div className="Projects--text-container">
-            <p className="Projects--name">Aeyos</p>
-            <p className="Projects--description">
-              An exciting deck-building adventure that focuses on four unique heroes with different cards and relics.
-            </p>
-            {/* <div className="Projects--roles-container">
-              <p>Roles</p>
-              <ul className="Projects--roles">
-                <li>Creating gameplay elements</li>
-                <li>Designing character cards and relics</li>
-                <li>Balancing character skillsets</li>
-                <li>Testing gameplay flow and loop</li>
-              </ul>
-            </div> */}
+        {projectData.map((data) => (
+          <div
+            key={data.title}
+            className="Projects--meta-container">
+            <div className="Projects--text-container">
+              <p className="Projects--name">{data.title}</p>
+              <p className="Projects--description">{data.description}</p>
+            </div>
+            <div className="Projects--visual-container">
+              <button className="Projects--arrow-btn Projects--arrow-btn-left">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 19.5L8.25 12l7.5-7.5"
+                  />
+                </svg>
+              </button>
+              <button className="Projects--arrow-btn Projects--arrow-btn-right">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                  />
+                </svg>
+              </button>
+              {data.imgPaths.map((path, index) => (
+                <div
+                  key={path}
+                  className="Projects--img-container"
+                  style={{ transform: "translateX(" + index * 102.56 + "%)" }}>
+                  <img
+                    className="Projects--img"
+                    src={path}
+                    alt="Aeyos"
+                  />
+                </div>
+              ))}
+            </div>
           </div>
-          <div className="Projects--visual-container">
-            <button className="Projects--arrow-btn Projects--arrow-btn-left">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 19.5L8.25 12l7.5-7.5"
-                />
-              </svg>
-            </button>
-            <button className="Projects--arrow-btn Projects--arrow-btn-right">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                />
-              </svg>
-            </button>
-            <img
-              className="Projects--img"
-              src="/img/projects/aeyos/aeyos-1.png"
-              alt="Aeyos"
-            />
-            {/* <img
-              className="Projects--img"
-              src="/img/projects/aeyos/aeyos-2.png"
-              alt="Aeyos"
-            />
-            <img
-              className="Projects--img"
-              src="/img/projects/aeyos/aeyos-3.png"
-              alt="Aeyos"
-            />
-            <img
-              className="Projects--img"
-              src="/img/projects/aeyos/aeyos-4.png"
-              alt="Aeyos"
-            />
-            <img
-              className="Projects--img"
-              src="/img/projects/aeyos/aeyos-5.png"
-              alt="Aeyos"
-            /> */}
-          </div>
-        </div>
+        ))}
       </main>
     </div>
   );
