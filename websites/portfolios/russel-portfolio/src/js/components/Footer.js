5b6cbfb928fa5926c6b99dc8b7ae366225b84b7e
sailecodes <eliasiv.codes@gmail.com>
2023-10-24 19:21:56 -0700

    init footer layout

--- websites/portfolios/russel-portfolio/src/js/components/Footer.js
+++ websites/portfolios/russel-portfolio/src/js/components/Footer.js
@@ -1,5 +1,25 @@
+import "../../css/Footer.css";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="Footer--container">
+      <footer className="Footer">
+        <p className="Footer--tagline">Want to connect?</p>
+        <div className="Footer--social-container">
+          <a
+            href="https://www.linkedin.com/in/russel-koh-44bb90122/"
+            target="_blank"
+            rel="noreferrer">
+            <img
+              className="Footer--social-img"
+              src="/img/socials/linkedin.png"
+              alt="Linkedin logo"
+            />
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
